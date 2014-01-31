e3fe6af98e8e6e016d4f636b9340ba6c52a4b562
Emily Foley & Ricardo A. Rubio <pair+EmFoley.rickarubio@devbootcamp.com>
2014-01-31 15:11:41 -0800

    created Deck model with initialize, generate_deck and card_count methods.

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,3 +1,19 @@
 class Deck < ActiveRecord::Base
-  # Remember to create a migration!
+  def initialize(params)
+    super
+    @deck = generate_deck
+    @deck.shuffle!
+  end
+
+  def generate_deck(deck_id)
+    Cards.where(deck_id: deck_id)
+  end
+
+  def top_card
+    @deck.pop
+  end
+
+  def card_count
+    @deck.count
+  end
 end
