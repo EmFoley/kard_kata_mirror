19713a4cf51514b7a2862fe5a8d76e40e0864ed7
Alexander Leishman <leishman3@gmail.com>
2014-01-31 14:31:11 -0800

    Created migration files

--- db/migrate/20140131123037_create_guesses.rb
+++ db/migrate/20140131123037_create_guesses.rb
@@ -1,4 +1,11 @@
 class CreateGuesses < ActiveRecord::Migration
   def change
+  	create_table :guesses do |t|
+  		t.integer :round_id
+  		t.integer :outcome  # 1 or 0
+  		t.integer :card_id
+
+  		t.timestamps
+  	end
   end
 end
