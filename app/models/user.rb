e97a73cdfad430e6c8a6000e090a2d1ee137311a
Matthew Higgins & Patrick Vilhena <pair+mh120888.nifengle@devbootcamp.com>
2014-01-31 15:15:00 -0800

    Comments in User model for future use in sign up pw confirmation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ActiveRecord::Base
+  # attr_accessor: user_entered_password
   has_many :decks
   validates :username, uniqueness: true
   validates :username, presence: true
   validates :password, presence: true
+  # validate :user_passwords_match
+
+  # def user_passwords_match
+  #   self.password == self.user_entered_password
+  # end
 end
+
