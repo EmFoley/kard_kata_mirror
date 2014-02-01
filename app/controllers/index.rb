fe199ab2a78090922315569d614915832876cdab
Matthew Higgins & Patrick Vilhena <pair+mh120888.nifengle@devbootcamp.com>
2014-01-31 20:41:33 -0800

    Created users controller, removed duplicate file. Pulled some verification logic into helper

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,54 +1,9 @@
 enable :sessions
 
-before do
-  @logged_in = session[:id] ? true : false
-end
-
 get '/' do
-  @valid_pw = session[:message]
+  @msg = session[:message]
   @action = "/login"
   @btn = "Login!"
   session[:message] = nil
   erb :index
-end
-
-get '/users/new' do
-  @action = "/users/new"
-  @btn = "Create User"
-  erb :new_user
-end
-
-get '/users/:id' do
-  if @logged_in
-    @decks = Deck.where(user_id: params[:id])
-    erb :logged_in_user
-  else
-    session[:message] = "You can't do that, you're not logged in!"
-    redirect '/'
-  end
-end
-
-post '/login' do
-  user = User.find_by_username(params[:username])
-  if params[:password] == user.password
-    session[:id] = user.id
-    redirect "/users/#{user.id}"
-  else
-    session[:message] = "Invalid Password"
-    redirect "/"
-  end
-end
-
-post '/logout' do
-  session[:id] = nil
-  redirect '/'
-end
-
-post '/users/new' do
-  new_user = User.new(params)
-  if new_user.valid?
-    new_user.save
-    session[:id] = new_user.id
-    redirect "/users/#{new_user.id}"
-  end
 end
---
+++ app/controllers/users.rb
@@ -0,0 +1,43 @@
+before do
+  @logged_in = session[:id] ? true : false
+end
+
+get '/users/new' do
+  @msg = session[:message]
+  @action = "/users/new"
+  @btn = "Create User"
+  erb :new_user
+end
+
+get '/users/:id' do
+  if @logged_in
+    @decks = Deck.where(user_id: params[:id])
+    erb :logged_in_user
+  else
+    session[:message] = "You can't do that, you're not logged in!"
+    redirect '/'
+  end
+end
+
+post '/login' do
+  user = User.find_by_username(params[:username])
+  check_password(user)
+end
+
+post '/logout' do
+  session[:id] = nil
+  redirect '/'
+end
+
+post '/users/new' do
+  new_user = User.new(params)
+  if approve_new_user(new_user)
+    redirect "/users/#{new_user.id}"
+  else
+    redirect 'users/new'
+  end
+end
+
+post '/users/decks' do
+  puts params
+end
