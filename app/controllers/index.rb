0d2fa337f7837327e772beb59f59be1b01de6527
Emily Foley & Ricardo A. Rubio <pair+EmFoley.rickarubio@devbootcamp.com>
2014-01-31 16:30:49 -0800

    developed initial card logic and routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,10 +7,20 @@ get '/info' do
   Demo.new(self).info
 end
 
-get '/decks/:deck_id/:user_id'
-  # @current_card = Deck.where(id: session[:deck_id])
-  # @current_card.pop
+get '/decks/:deck_id' do
+  @message = session[:answer_message]
+  @card = Card.where(deck_id: params[:deck_id]).sample
   erb :game
+  session[:answer_message] = ""
 end
 
+post '/cards/:card_id' do
+  card = Card.find(params[:card_id])
+  if params[:user_answer] == card.answer
+    session[:answer_message] = "Congratulations! That is correct"
+  else
+    session[:answer_message] = "Sorry, that was not correct :("
+  end
+  redirect "/decks/#{card.deck_id}"
+end
 # select a deck page -> array of card objects -> session[:cards_array]
